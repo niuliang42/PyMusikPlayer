b08dd5fab8340b13c7bd69bae5ebbcf83758ea10
Wizmann <mail.kuuy@gmail.com>
2012-06-24 00:36:01 +0800

    Add commit and TODO is create a view of all the tables to make it easy when i need a shuffle item in the database

--- DBControl.py
+++ DBControl.py
@@ -20,10 +20,14 @@ class DBControl():
 				key not null,\
 				path not null\
 			);')
+		self.db.commit()
+		self.cur.close()
+	
 	def add_new_path(self,i_path,force=False):
 		md5x=md5()
 		md5x.update(i_path)
 		encode_path=md5x.hexdigest()[8:-8]
+		encode_path='T_'+encode_path
 		print self.table_exists(encode_path)
 		if(force):
 			if(self.table_exists(encode_path)):
@@ -42,27 +46,45 @@ class DBControl():
 		print 'Add_new_path OK!'
 		
 	def create_table(self,table_name):
+		self.cur=self.db.cursor()
 		self.cur.execute(\
 			'CREATE TABLE IF NOT EXISTS '+table_name+\
 			'(\
-				musik_path TEXT NOT NULL\
+				musik_path TEXT NOT NULL,\
+				UNIQUE (\'musik_path\')\
 			);')
+		self.db.commit()
+		self.cur.close()
 	
 	def drop_table(self,table_name):
+		self.cur=self.db.cursor()
 		self.cur.execute('DROP TABLE \''+table_name+'\'')
+		self.cur.close()
 	
 	def table_exists(self,table_name):
+		self.cur=self.db.cursor()
 		self.cur.execute(\
 			'SELECT COUNT(*) FROM sqlite_master \
 				where type=\'table\' and name=\''+table_name+'\';')
 		num=self.cur.fetchone()[0]
+		self.cur.close()
 		return True if num>0 else False
 		
 	def insertDB(self,table_name,value):
-		self.cur.execute('INSERT INTO '+table_name+' (musik_path) VALUES '+'(\"'+value+'\");')
+		self.cur=self.db.cursor()
+		self.cur.execute('INSERT INTO '+ '\"' +table_name + '\"' + ' (musik_path) VALUES \n'+'(\"'+value+'\");')
+		self.db.commit()
+		self.cur.close()
+	 
+	def __del__(self):
+		self.db.close()
 
 
 if(__name__=='__main__'):
 	db=DBControl()
-	db.add_new_path('/home/wizmann/音乐',force=True)
+	db.add_new_path('/home/wizmann/音乐/B',force=True)
+	db.add_new_path('/home/wizmann/音乐/A',force=True)
+	
+	db.create_all_view()
+	
 
